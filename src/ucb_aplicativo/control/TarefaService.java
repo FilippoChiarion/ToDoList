9809e8d703a1b75686a9d9c49d8ef8b96241621e
Gabrilex2 <gabriel.alves.santana.12@gmail.com>
2025-09-09 20:30:07 -0300

    control criado

--- src/ucb_aplicativo/control/TarefaService.java
+++ src/ucb_aplicativo/control/TarefaService.java
@@ -1,4 +1,59 @@
 package ucb_aplicativo.control;
+import ucb_aplicativo.model.Tarefa;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TarefaService {
+    private List<Tarefa> tarefas = new ArrayList<>();
+    private Long id = 1L;
+
+    public Tarefa criar(String titulo, String descricao) {
+        Tarefa novaTarefa = new Tarefa(id++, titulo, descricao, false);
+        tarefas.add(novaTarefa);
+        return novaTarefa;
+    }
+
+    public List<Tarefa> listar() {
+        return tarefas;
+    }
+
+    public boolean atualizar(Long id, String novoTitulo, String novaDescricao) {
+        for (Tarefa teste : tarefas) {
+            if (teste.getId().equals(id)) {
+                teste.setTitulo(novoTitulo);
+                teste.setDescricao(novaDescricao);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean remover(Long id) {
+        for (Tarefa tarefa : tarefas) {
+            if (tarefa.getId().equals(id)) {
+                tarefas.remove(tarefa);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean marcar(Long id) {
+        for (Tarefa tarefa : tarefas) {
+            if (tarefa.getId().equals(id)) {
+                tarefa.setCompleta(true);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Tarefa pesquisar(Long id) {
+        for (Tarefa tarefa : tarefas) {
+            if (tarefa.getId().equals(id)) {
+                return tarefa;
+            }
+        }
+        return null;
+    }
 }
