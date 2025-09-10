481a6b477046fa159a07528565c335b8dcdde0b1
GabrielLucas-dev <dev.gabriellucas@gmail.com>
2025-09-10 09:37:44 -0300

    view criada

--- src/ucb_aplicativo/cli/View.java
+++ src/ucb_aplicativo/cli/View.java
@@ -1,7 +1,26 @@
 package ucb_aplicativo.cli;
+import ucb_aplicativo.control.TarefaService;
+import ucb_aplicativo.model.Tarefa;
+import java.util.List;
+import java.util.Scanner;
 
 public class View {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+
+        TarefaService servico = new TarefaService();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n===== To Do List =====");
+            System.out.println("1. Criar tarefa");
+            System.out.println("2. Listar tarefas");
+            System.out.println("3. Pesquisar tarefa");
+            System.out.println("4. Atualizar tarefa");
+            System.out.println("5. Remover tarefa");
+            System.out.println("6. Marcar como concluída");
+            System.out.println("7. Listar tarefas concluídas");
+            System.out.println("8. Sair");
+            System.out.print("Escolha uma opção: ");
+
+            int opcao = sc.nextInt();
+            sc.nextLine();
